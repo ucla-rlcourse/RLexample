Fix a bug in the baseline computation of policy gradient methods (#13)
* fix a gpu bug

* detach the gradient in ac-pong-pytorch

* detach gradient in PG-baseline

* update dependency claim

* update a config

* update a config

* format the code

policygradient/pgb-pong-pytorch.py
@@ -1,19 +1,19 @@
 # Pytorch implementation of PG with baseline
 #
 # Bolei Zhou, 22 Feb 2019
-import os
+# PENG Zhenghao, updated 23 October 2021
 import argparse
-import gym
-import numpy as np
+import os
 from itertools import count
 
+import gym
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.distributions import Categorical
-import pdb
 
 is_cuda = torch.cuda.is_available()
 
@@ -29,10 +29,13 @@ parser.add_argument('--batch_size', type=int, default=20, metavar='G',
 parser.add_argument('--seed', type=int, default=87, metavar='N',
                     help='random seed (default: 87)')
 parser.add_argument('--test', action='store_true',
-        help='whether to test the trained model or keep training')
+                    help='whether to test the trained model or keep training')
+parser.add_argument('--use_value_gradient', action='store_true',
+                    help='whether to pass gradient to value network when computing policy loss. Test purpose only!')
+parser.add_argument('--disable_model_loading', action='store_true',
+                    help='whether to skip the model loading process. Test purpose only!')
 
 args = parser.parse_args()
-
 
 env = gym.make('Pong-v0')
 env.seed(args.seed)
@@ -40,10 +43,11 @@ torch.manual_seed(args.seed)
 
 D = 80 * 80
 test = args.test
-if test ==True:
+if test == True:
     render = True
 else:
     render = False
+
 
 def prepro(I):
     """ prepro 210x160x3 into 6400 """
@@ -51,7 +55,7 @@ def prepro(I):
     I = I[::2, ::2, 0]
     I[I == 144] = 0
     I[I == 109] = 0
-    I[I != 0 ] = 1
+    I[I != 0] = 1
     return I.astype(np.float).ravel()
 
 
@@ -59,7 +63,7 @@ class PGbaseline(nn.Module):
     def __init__(self, num_actions=2):
         super(PGbaseline, self).__init__()
         self.affine1 = nn.Linear(6400, 200)
-        self.action_head = nn.Linear(200, num_actions) # action 1: static, action 2: move up, action 3: move down
+        self.action_head = nn.Linear(200, num_actions)  # action 1: static, action 2: move up, action 3: move down
         self.value_head = nn.Linear(200, 1)
 
         self.num_actions = num_actions
@@ -72,7 +76,6 @@ class PGbaseline(nn.Module):
         state_values = self.value_head(x)
         return F.softmax(action_scores, dim=-1), state_values
 
-
     def select_action(self, x):
         x = Variable(torch.from_numpy(x).float().unsqueeze(0))
         if is_cuda: x = x.cuda()
@@ -83,22 +86,23 @@ class PGbaseline(nn.Module):
         self.saved_log_probs.append((m.log_prob(action), state_value))
         return action
 
+
 # built policy network
 policy = PGbaseline()
 if is_cuda:
     policy.cuda()
 
 # check & load pretrain model
-if os.path.isfile('pgb_params.pkl'):
+if os.path.isfile('pgb_params.pkl') and (not args.disable_model_loading):
     print('Load PGbaseline Network parametets ...')
     if is_cuda:
         policy.load_state_dict(torch.load('pgb_params.pkl'))
     else:
         policy.load_state_dict(torch.load('pgb_params.pkl', map_location=lambda storage, loc: storage))
 
-
 # construct a optimal function
 optimizer = optim.RMSprop(policy.parameters(), lr=args.learning_rate, weight_decay=args.decay_rate)
+
 
 def finish_episode():
     R = 0
@@ -111,11 +115,16 @@ def finish_episode():
     # turn rewards to pytorch tensor and standardize
     rewards = torch.Tensor(rewards)
     rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-6)
-    if is_cuda: rewards = rewards.cuda()
+    if is_cuda:
+        rewards = rewards.cuda()
     for (log_prob, value), reward in zip(policy.saved_log_probs, rewards):
         advantage = reward - value
-        policy_loss.append(- log_prob * advantage)         # policy gradient
-        value_loss.append(F.smooth_l1_loss(value, reward)) # value function approximation
+        if args.use_value_gradient:
+            pass
+        else:
+            advantage = advantage.detach()
+        policy_loss.append(- log_prob * advantage)  # policy gradient
+        value_loss.append(F.smooth_l1_loss(value, reward))  # value function approximation
     optimizer.zero_grad()
     policy_loss = torch.stack(policy_loss).sum()
     value_loss = torch.stack(value_loss).sum()
@@ -150,19 +159,16 @@ for i_episode in count(1):
         if done:
             # tracking log
             running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
-            print('Policy Gradient with Baseline ep %03d done. reward: %f. reward running mean: %f' % (i_episode, reward_sum, running_reward))
+            print('Policy Gradient with Baseline ep %03d done. reward: %f. reward running mean: %f' % (
+                i_episode, reward_sum, running_reward))
             reward_sum = 0
             break
-
 
     # use policy gradient update model weights
     if i_episode % args.batch_size == 0 and test == False:
         finish_episode()
 
     # Save model in every 50 episode
-    if i_episode % 50 == 0 and test == False:
+    if (i_episode % 50 == 0) and (test == False) and (not args.disable_model_loading):
         print('ep %d: model saving...' % (i_episode))
         torch.save(policy.state_dict(), 'pgb_params.pkl')
-
-
-