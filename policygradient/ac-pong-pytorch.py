fix policy gradient

policygradient/ac-pong-pytorch.py
@@ -5,7 +5,7 @@ import argparse
 import os
 from itertools import count
 
-import gym
+import gymnasium as gym
 import numpy as np
 import torch
 import torch.nn as nn
@@ -33,10 +33,6 @@ parser.add_argument('--max-grad-norm', type=float, default=10)
 parser.add_argument('--value-loss-coef', type=float, default=0.5)
 args = parser.parse_args()
 
-env = gym.make('Pong-v0')
-env.seed(args.seed)
-torch.manual_seed(args.seed)
-
 D = 80 * 80
 test = args.test
 if test == True:
@@ -44,6 +40,11 @@ if test == True:
 else:
     render = False
 
+if render:
+    env = gym.make('Pong-v0', render_mode='human')
+else:
+    env = gym.make('Pong-v0')
+torch.manual_seed(args.seed)
 
 def prepro(I):
     """ prepro 210x160x3 into 6400 """
@@ -52,7 +53,7 @@ def prepro(I):
     I[I == 144] = 0
     I[I == 109] = 0
     I[I != 0] = 1
-    return I.astype(np.float).ravel()
+    return I.astype(np.float32).ravel()
 
 
 class AC(nn.Module):
@@ -140,18 +141,18 @@ def finish_episode():
 running_reward = None
 reward_sum = 0
 for i_episode in count(1):
-    state = env.reset()
+    state, _ = env.reset(seed=args.seed)
     prev_x = None
     policy.rewards.append([])  # record rewards separately for each episode
     policy.saved_log_probs.append([])
     for t in range(10000):
-        if render: env.render()
         cur_x = prepro(state)
         x = cur_x - prev_x if prev_x is not None else np.zeros(D)
         prev_x = cur_x
         action = policy.select_action(x)
         action_env = action + 2
-        state, reward, done, _ = env.step(action_env)
+        state, reward, terminated, truncated, _ = env.step(action_env)
+        done = np.logical_or(terminated, truncated)
         reward_sum += reward
 
         policy.rewards[-1].append(reward)

policygradient/pg-pong-pytorch.py
@@ -5,7 +5,7 @@
 
 import os
 import argparse
-import gym
+import gymnasium as gym
 import numpy as np
 from itertools import count
 
@@ -36,8 +36,7 @@ parser.add_argument('--test', action='store_true',
 args = parser.parse_args()
 
 
-env = gym.make('Pong-v0')
-env.seed(args.seed)
+# env.seed(args.seed)
 torch.manual_seed(args.seed)
 
 D = 80 * 80
@@ -46,6 +45,11 @@ if test ==True:
     render = True
 else:
     render = False
+
+if render:
+    env = gym.make('Pong-v0', render_mode='human')
+else:
+    env = gym.make('Pong-v0')
 
 def prepro(I):
     """ prepro 210x160x3 into 6400 """
@@ -130,15 +134,15 @@ running_reward = None
 reward_sum = 0
 prev_x = None
 for i_episode in count(1):
-    state = env.reset()
+    state, _ = env.reset()
     for t in range(10000):
-        if render: env.render()
         cur_x = prepro(state)
         x = cur_x - prev_x if prev_x is not None else np.zeros(D)
         prev_x = cur_x
         action = policy.select_action(x)
         action_env = action + 2
-        state, reward, done, _ = env.step(action_env)
+        state, reward, terminated, truncated, _ = env.step(action_env)
+        done = np.logical_or(terminated, truncated)
         reward_sum += reward
 
         policy.rewards.append(reward)

policygradient/pgb-pong-pytorch.py
@@ -6,7 +6,7 @@ import argparse
 import os
 from itertools import count
 
-import gym
+import gymnasium as gym
 import numpy as np
 import torch
 import torch.nn as nn
@@ -38,7 +38,6 @@ parser.add_argument('--disable_model_loading', action='store_true',
 args = parser.parse_args()
 
 env = gym.make('Pong-v0')
-env.seed(args.seed)
 torch.manual_seed(args.seed)
 
 D = 80 * 80
@@ -56,7 +55,7 @@ def prepro(I):
     I[I == 144] = 0
     I[I == 109] = 0
     I[I != 0] = 1
-    return I.astype(np.float).ravel()
+    return I.astype(np.float32).ravel()
 
 
 class PGbaseline(nn.Module):
@@ -143,7 +142,7 @@ def finish_episode():
 running_reward = None
 reward_sum = 0
 for i_episode in count(1):
-    state = env.reset()
+    state, _ = env.reset(seed=args.seed)
     prev_x = None
     for t in range(10000):
         if render: env.render()
@@ -152,7 +151,8 @@ for i_episode in count(1):
         prev_x = cur_x
         action = policy.select_action(x)
         action_env = action + 2
-        state, reward, done, _ = env.step(action_env)
+        state, reward, terminated, truncated, _ = env.step(action_env)
+        done = np.logical_or(terminated, truncated)
         reward_sum += reward
 
         policy.rewards.append(reward)

policygradient/reinforce.py
@@ -1,5 +1,5 @@
 import argparse
-import gym
+import gymnasium as gym
 import numpy as np
 from itertools import count
 
@@ -22,8 +22,10 @@ parser.add_argument('--log-interval', type=int, default=10, metavar='N',
 args = parser.parse_args()
 
 
-env = gym.make('CartPole-v1')
-env.seed(args.seed)
+if args.render:
+    env = gym.make('CartPole-v1', render_mode='human')
+else:
+    env = gym.make('CartPole-v1')
 torch.manual_seed(args.seed)
 
 
@@ -81,10 +83,12 @@ def finish_episode():
 def main():
     running_reward = 10
     for i_episode in count(1):
-        state, ep_reward = env.reset(), 0
+        state, _ = env.reset(seed=args.seed)
+        ep_reward = 0
         for t in range(1, 10000):  # Don't infinite loop while learning
             action = select_action(state)
-            state, reward, done, _ = env.step(action)
+            state, reward, terminated, truncated, _ = env.step(action)
+            done = np.logical_or(terminated, truncated)
             if args.render:
                 env.render()
             policy.rewards.append(reward)