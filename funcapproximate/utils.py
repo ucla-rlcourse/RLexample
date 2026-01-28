210c6a8345ffefc9bb2a4d71b9e689a658399b2a
Haoyuan Cai <haoyuan@cs.ucla.edu>
2026-01-27 17:21:05 -0800

    Upload mountain_car.ipynb

---
+++ funcapproximate/utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def argmax(q_values):
+    top = float("-inf")
+    ties = []
+
+    for i in range(len(q_values)):
+        if q_values[i] > top:
+            top = q_values[i]
+            ties = []
+
+        if q_values[i] == top:
+            ties.append(i)
+
+    return np.random.choice(ties)
